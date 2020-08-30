b8421464e714d0640d54d2eb85762a0f18dea77c
Rishabh Kumar <rishabhk3003@gmail.com>
2020-08-30 14:23:12 +0530

    added user entries list and balance

---
+++ components/UserEntry.tsx
@@ -0,0 +1,67 @@
+import * as React from "react"
+import {View, Text, StyleSheet} from "react-native"
+import moment from 'moment'
+
+interface IUserEntry {
+    details: any
+}
+
+const UserEntry = (props: IUserEntry) => {
+    const {details} = props
+    const isDebit = details.type === "DEBIT"
+    const date = moment(details.created_at).format("DD-MM-YYYY")
+    return (
+        <View style={styles.container}>
+            <View>
+                <View style={styles.inline}>
+                    <Text style={styles.header}>{details.name}</Text>
+                    <Text style={styles.date}>{date}</Text>
+                </View>
+                <Text style={styles.subHeader}>{details.description || ""}</Text>
+            </View>
+            <View>
+                <Text style={isDebit ? styles.debitAmount : styles.creditAmount}>{details.amount}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 10,
+        paddingRight: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    inline: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    header: {
+        color: '#DEF1F8',
+        fontSize: 14
+    },
+    subHeader: {
+        color: '#DEF1F8',
+        opacity: 0.8,
+        fontSize: 10
+    },
+    debitAmount: {
+        color: '#FE6D73',
+        fontSize: 12
+    },
+    creditAmount: {
+        color: '#8BC795',
+        fontSize: 12
+    },
+    date: {
+        fontSize: 10,
+        paddingLeft: 10,
+        color: '#DEF1F8'
+    }
+})
+
+export default UserEntry
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,33 +1,47 @@
 import * as React from "react"
-import { StyleSheet, TextInput, Button, View, Text } from 'react-native';
+import { StyleSheet, Modal, TouchableOpacity, View, Text, FlatList, Platform } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { AuthContext } from "../context/authContext";
 import { isEmpty as _isEmpty } from "lodash";
 import { FETCH_USER_ENTRIES_URL } from "../appConstants";
 import { fetchHelper } from "../utils";
+import UserEntry from "../components/UserEntry";
 
 const HomeScreen = () => {
     const {user, auth} = React.useContext(AuthContext)
     const [isAppLoading, setIsAppLoading] = React.useState<boolean>(true)
-    const [entries, setEntries] = React.useState<Array<Object>>([])
+    const [entries, setEntries] = React.useState<Array<any>>([])
+    const [refresh, setRefresh] = React.useState<boolean>(false)
+    const [openModal, setOpenModal] = React.useState<boolean>(false)
+
+    const fetchUserEntries = async () => {
+        const {data} = await fetchHelper(FETCH_USER_ENTRIES_URL, {
+            method: 'GET',
+            queryParams: {
+                userId: user.id
+            }
+        })
+        setIsAppLoading(false)
+        // console.log({userEntriesData: data})
+        setEntries(data)
+    }
 
     // cmd
     React.useEffect(() => {
-        const fetchUserEntries = async () => {
-            const {data} = await fetchHelper(FETCH_USER_ENTRIES_URL, {
-                method: 'GET',
-                queryParams: {
-                    userId: user.id
-                }
-            })
-            setIsAppLoading(false)
-            console.log({userEntriesData: data})
-            setEntries(data)
-        }
-
         fetchUserEntries()
     }, [])
 
+    const handleRefresh = async () => {
+        setRefresh(true)
+        await fetchUserEntries()
+        setRefresh(false)
+    }
+
+    const handleItemLongPress = () => {
+        setOpenModal(true)
+    }
+
+    const hasEntries = !_isEmpty(entries)
     return (
         <View style={styles.container}>
             {/* <Text style={styles.logout} onPress={auth.logout}>Logout</Text> */}
@@ -35,18 +49,59 @@ const HomeScreen = () => {
                 <Text>
                     {`Welcome ${user.name}`}
                 </Text>
+                <Text>
+                    {hasEntries && `Balance ${entries[0].balance}`}
+                </Text>
             </View>
             <Animatable.View animation="fadeInUpBig" style={styles.footer}>
-            {isAppLoading && <Text>{"Loading Entries..."}</Text>}
-            {!isAppLoading && <View>
-                {
-                    !_isEmpty(entries)
-                        ? entries.map((userEntry: any) => {
-                            return <View></View>
-                        })
-                        : <Text>{"No Entries yet!"}</Text>
-                }
-            </View>}
+                {isAppLoading && <Text>{"Loading Entries..."}</Text>}
+                {!isAppLoading && <View>
+                    {
+                        <FlatList 
+                            ItemSeparatorComponent={() =>
+                                // Platform.OS !== 'android' &&
+                                // (({ highlighted }) => (
+                                <View
+                                    style={{
+                                        width: '100%',
+                                        height: 0.5,
+                                        backgroundColor: '#f7fcfe',
+                                        opacity: 0.5
+                                    }}
+                                />
+                                // ))
+                            }
+                            ListEmptyComponent={() => <Text>{"No Entries yet!"}</Text>}
+                            showsHorizontalScrollIndicator={false}
+                            data={entries}
+                            refreshing={refresh}
+                            onRefresh={handleRefresh}
+                            keyExtractor={(item: any, index: number) => `${item.name}-${index}`}
+                            renderItem={({item, index, separators}) => (
+                            <TouchableOpacity
+                                activeOpacity={0.8}
+                                onLongPress={handleItemLongPress}
+                            >
+                                <UserEntry details={item} />
+                            </TouchableOpacity>)}
+                        />
+                    }
+                </View>}
+                <Modal
+                    visible={openModal}
+                    transparent={false}
+                    animationType={"slide"}
+                >
+                    <Text>{"Modal HERE"}</Text>
+                    <TouchableOpacity
+                        style={{ backgroundColor: "#2196F3" }}
+                        onPress={() => {
+                            setOpenModal(false);
+                        }}
+                        >
+                        <Text>Hide Modal</Text>
+                    </TouchableOpacity>
+                </Modal>
             </Animatable.View>
         </View>
     )
@@ -55,7 +110,7 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        backgroundColor: '#227C9D'
+        backgroundColor: '#DEF1F8'
     },
     header: {
         flex: 1,
@@ -64,8 +119,8 @@ const styles = StyleSheet.create({
         paddingBottom: 50
     },
     footer: {
-        flex: 4,
-        backgroundColor: '#DEF1F8',
+        flex: 5,
+        backgroundColor: '#113D4D',
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
         paddingHorizontal: 20,
